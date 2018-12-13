64ac3a27ca4708835c657a671518e05b975400f5
VBrazhnik <vbrazhnikdev@gmail.com>
2018-12-13 05:58:39 +0200

    free_command_list() and free_stack() were updated

--- sources/free.c
+++ sources/free.c
@@ -16,13 +16,15 @@
 void	free_command_list(t_command_list *list)
 {
 	t_command *current;
+	t_command *delete;
 
 	current = list->head;
 	while (current)
 	{
-		free(current->name);
-		free(current);
+		delete = current;
 		current = current->next;
+		free(delete->name);
+		free(delete);
 	}
 	free(list);
 }
@@ -31,13 +33,15 @@ void	free_stack(t_stack *stack)
 {
 	size_t			i;
 	t_stack_elem	*current;
+	t_stack_elem	*delete;
 
 	i = 0;
 	current = stack->head;
 	while (i < stack->size)
 	{
-		free(current);
+		delete = current;
 		current = current->next;
+		free(delete);
 		i++;
 	}
 	free(stack);
